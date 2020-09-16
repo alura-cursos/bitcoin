42b41b70648b79ae972486660d4a388b76eb9c11
João Victor <jvc.martins12@gmail.com>
2020-09-16 11:37:58 -0300

    Configurando a aplicação para requisição à API

---
+++ src/main/java/br/com/alura/model/Bitcoin.java
@@ -0,0 +1,15 @@
+package br.com.alura.model;
+
+import java.time.LocalDate;
+
+public class Bitcoin {
+	
+	private Long id;
+	
+	private Double preco;
+	
+	private String tipo;
+	
+	private LocalDate data;
+
+}
---
+++ src/main/java/br/com/alura/service/BitcoinService.java
@@ -0,0 +1,22 @@
+package br.com.alura.service;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import br.com.alura.model.Bitcoin;
+
+@Path("/bitcoins")
+@RegisterRestClient
+public interface BitcoinService {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Bitcoin> listar();
+
+}
