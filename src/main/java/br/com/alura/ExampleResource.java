c16e95a1aca6821085512dc0d80c08207adafa9e
João Victor <jvc.martins12@gmail.com>
2020-09-16 17:35:15 -0300

    Ajustes feitos na regravação

--- src/main/java/br/com/alura/ExampleResource.java
+++ src/main/java/br/com/alura/ExampleResource.java
@@ -8,9 +8,10 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class ExampleResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
-    }
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		return "hello";
+	}
+
 }
--- src/main/java/br/com/alura/model/Bitcoin.java
+++ src/main/java/br/com/alura/model/Bitcoin.java
@@ -3,13 +3,13 @@ package br.com.alura.model;
 import java.time.LocalDate;
 
 public class Bitcoin {
-	
+
 	private Long id;
-	
+
 	private Double preco;
-	
+
 	private String tipo;
-	
+
 	private LocalDate data;
 
 }
