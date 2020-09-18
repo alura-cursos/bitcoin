050092988ac736f0f32cc6f8851f16bab526a883
João Victor <jvc.martins12@gmail.com>
2020-09-18 18:29:20 -0300

    Exemplo da entidade Ordem

---
+++ src/main/java/br/com/alura/model/Ordem.java
@@ -0,0 +1,48 @@
+package br.com.alura.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Ordem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private Double preco;
+
+	private String tipo;
+
+	private LocalDate data;
+
+	private String status;
+
+	@Column(name = "user_id")
+	private Long userId;
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+}
