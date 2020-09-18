32e2963de0307368a513b59769ad51eabb6e83bd
João Victor <jvc.martins12@gmail.com>
2020-09-18 14:55:36 -0300

    Configuração do Datasource e Entidade de usuario

---
+++ src/main/java/br/com/alura/model/Usuario.java
@@ -0,0 +1,38 @@
+package br.com.alura.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nome;
+
+	private String cpf;
+
+	private String username;
+
+	private String password;
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
