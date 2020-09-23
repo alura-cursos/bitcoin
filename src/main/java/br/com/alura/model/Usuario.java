abbc3dc1cc04d9282afb7266a8221d630b1d1c74
João Victor <jvc.martins12@gmail.com>
2020-09-23 14:02:50 -0300

    Aplicando regras de segurança para usuário

--- src/main/java/br/com/alura/model/Usuario.java
+++ src/main/java/br/com/alura/model/Usuario.java
@@ -5,9 +5,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import io.quarkus.elytron.security.common.BcryptUtil;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.security.jpa.Password;
+import io.quarkus.security.jpa.Roles;
+import io.quarkus.security.jpa.UserDefinition;
+import io.quarkus.security.jpa.Username;
 
 @Entity
+@UserDefinition
 public class Usuario extends PanacheEntityBase {
 
 	@Id
@@ -18,10 +24,13 @@ public class Usuario extends PanacheEntityBase {
 
 	private String cpf;
 
+	@Username
 	private String username;
 
+	@Password
 	private String password;
 
+	@Roles
 	private String role;
 
 	public void setNome(String nome) {
@@ -36,11 +45,35 @@ public class Usuario extends PanacheEntityBase {
 		this.username = username;
 	}
 
+	public String getUsername() {
+		return username;
+	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	public String getPassword() {
+		return password;
+	}
+
 	public void setRole(String role) {
 		this.role = role;
+	}
+
+	public String getRole() {
+		return role;
+	}
+	
+	public static void adicionar(Usuario usuario) {
+		usuario.password = BcryptUtil.bcryptHash(usuario.password);
+		usuario.role = validarUsername(usuario.username);
+		usuario.persist();
+	}
+	
+	private static String validarUsername(String username) {
+		if(username.equals("alura")) {
+			return "admin";
+		} return "user";
 	}
 }
--- src/main/java/br/com/alura/resource/UsuarioResource.java
+++ src/main/java/br/com/alura/resource/UsuarioResource.java
@@ -1,5 +1,6 @@
 package br.com.alura.resource;
 
+import javax.annotation.security.PermitAll;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -12,10 +13,11 @@ import br.com.alura.model.Usuario;
 public class UsuarioResource {
 
 	@POST
+	@PermitAll
 	@Transactional
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void inserir(Usuario usuario) {
-		Usuario.persist(usuario);
+		Usuario.adicionar(usuario);
 	}
 
 }
